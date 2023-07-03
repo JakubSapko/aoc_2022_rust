1d1fafd0d82d133561ef06cc83a922cd854ebb5f
JakubSapko <jakub.sapko1@gmail.com>
2023-07-03 10:08:35 +0200

    add: d2.1

---
+++ aoc/src/day1/day1.rs
@@ -0,0 +1,39 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+fn main() {
+    let lines = read_lines("elves_foods.txt");
+    let mut temp = 0;
+    let mut output = [0; 3];
+    match lines {
+        Ok(lines) => {
+            for line in lines {
+                match line {
+                    Ok(line) => {
+                        if !line.is_empty() {
+                            temp = temp + line.parse::<i32>().unwrap();
+                        } else {
+                            if output.iter().any(|&x| temp > x) {
+                                output.sort();
+                                output[0] = temp;
+                            }
+                            temp = 0;
+                        }
+                    }
+                    Err(_) => println!("Error"),
+                }
+            }
+        }
+        Err(_) => println!("File not found"),
+    }
+    println!("{:?}", output.iter().sum::<i32>())
+}
--- aoc/src/main.rs
+++ aoc/src/main.rs
@@ -10,30 +10,32 @@ where
     Ok(io::BufReader::new(file).lines())
 }
 
+fn determine_score(line: &str) -> i32 {
+    match line {
+        "AY" => return 6 + 2,
+        "BZ" => return 6 + 3,
+        "CX" => return 6 + 1,
+        "AX" => return 3 + 1,
+        "BY" => return 3 + 2,
+        "CZ" => return 3 + 3,
+        "AZ" => return 0 + 3,
+        "BX" => return 0 + 1,
+        "CY" => return 0 + 2,
+        _ => return 0,
+    };
+}
+
 fn main() {
-    let lines = read_lines("elves_foods.txt");
-    let mut temp = 0;
-    let mut output = [0; 3];
+    let lines = read_lines("rockpaperscissors.txt");
     match lines {
         Ok(lines) => {
-            for line in lines {
-                match line {
-                    Ok(line) => {
-                        if !line.is_empty() {
-                            temp = temp + line.parse::<i32>().unwrap();
-                        } else {
-                            if output.iter().any(|&x| temp > x) {
-                                output.sort();
-                                output[0] = temp;
-                            }
-                            temp = 0;
-                        }
-                    }
-                    Err(_) => println!("Error"),
-                }
-            }
+            let values: i32 = lines
+                .into_iter()
+                .map(|line| line.unwrap().split_whitespace().collect::<String>())
+                .map(|line| determine_score(&line))
+                .sum();
+            println!("{}", values)
         }
-        Err(_) => println!("File not found"),
+        Err(_) => {}
     }
-    println!("{:?}", output.iter().sum::<i32>())
 }
