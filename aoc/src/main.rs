29e42a3980663ece9f7a1bcb21362f442ff31246
JakubSapko <jakub.sapko1@gmail.com>
2023-07-04 21:14:46 +0200

    add: d2.1

--- aoc/src/main.rs
+++ aoc/src/main.rs
@@ -10,35 +10,45 @@ where
     Ok(io::BufReader::new(file).lines())
 }
 
-fn determine_score(line: &str) -> i32 {
-    // X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win.
-    // A(1) - rock, B(2) - paper, C(3) - scissors
-    // lost - 0, draw - 3, win - 6
-    match line {
-        "AY" => return 1 + 3,
-        "AZ" => return 2 + 6,
-        "AX" => return 3 + 0,
-        "BY" => return 2 + 3,
-        "BZ" => return 3 + 6,
-        "BX" => return 1 + 0,
-        "CY" => return 3 + 3,
-        "CZ" => return 1 + 6,
-        "CX" => return 2 + 0,
-        _ => return 0,
-    };
+fn find_the_same_char(backpack: &Result<String, std::io::Error>) -> char {
+    let backpack = backpack.as_ref().unwrap();
+    let backpack_length = backpack.len();
+    let (first, last) = backpack.split_at(backpack_length / 2);
+    let mut found_char = ' ';
+    for (l) in first.chars() {
+        for (r) in last.chars() {
+            if l == r {
+                found_char = l;
+            }
+        }
+    }
+    found_char
 }
 
+fn backpack_to_int(found_item: char) -> i32 {
+    match found_item as u8 {
+        b'a'..=b'z' => (found_item as usize - b'a' as usize + 1)
+            .try_into()
+            .unwrap(),
+        b'A'..=b'Z' => (found_item as usize - b'A' as usize + 27)
+            .try_into()
+            .unwrap(),
+        _ => 0,
+    }
+}
 fn main() {
-    let lines = read_lines("rockpaperscissors.txt");
-    match lines {
-        Ok(lines) => {
-            let values: i32 = lines
-                .into_iter()
-                .map(|line| line.unwrap().split_whitespace().collect::<String>())
-                .map(|line| determine_score(&line))
-                .sum();
-            println!("{}", values)
+    let file = read_lines("elves_backpacks.txt");
+    match file {
+        Ok(file) => {
+            let lines = file.collect::<Vec<_>>();
+            let d1 = lines
+                .iter()
+                .map(|line| find_the_same_char(line))
+                .map(|line| backpack_to_int(line));
+            println!("{:?}", d1.sum::<i32>());
         }
-        Err(_) => {}
+        Err(_) => {
+            println!("error")
+        }
     }
 }
