bca763f4c9d5a7102e749a7004d61b21dfb0b591
JakubSapko <jakub.sapko1@gmail.com>
2023-07-10 09:51:00 +0200

    add: part one

--- aoc/src/main.rs
+++ aoc/src/main.rs
@@ -2,6 +2,28 @@ use std::path::PathBuf;
 
 use hashbrown::{HashMap, HashSet};
 
+fn calculate_dir_size(
+    key: &PathBuf,
+    files: &HashMap<PathBuf, HashSet<(i64, &str)>>,
+    dir_sizes: &mut HashMap<PathBuf, i64>,
+) {
+    if dir_sizes.contains_key(key) {
+        return;
+    }
+    let size = files[key]
+        .iter()
+        .map(|&(s, d)| match s {
+            -1 => {
+                let dir = key.join(d);
+                calculate_dir_size(&dir, files, dir_sizes);
+                dir_sizes[&dir]
+            }
+            s => s,
+        })
+        .sum();
+    dir_sizes.insert(key.clone(), size);
+}
+
 fn main() {
     let input = include_str!("osinstructions.txt");
     let mut files = HashMap::<_, HashSet<_>>::new();
@@ -21,5 +43,13 @@ fn main() {
             cd_dir => pwd.push(cd_dir.split_once(' ').unwrap().1),
         }
     }
-    println!("{:#?}", files)
+    println!("{:#?}", files);
+    let mut dir_sizes = HashMap::new();
+    for key in files.keys() {
+        calculate_dir_size(key, &files, &mut dir_sizes);
+    }
+    let size = dir_sizes[&PathBuf::from("/")];
+    println!("{}", size);
+    let p1 = dir_sizes.values().filter(|&&s| s <= 100000).sum::<i64>();
+    println!("{}", p1)
 }
